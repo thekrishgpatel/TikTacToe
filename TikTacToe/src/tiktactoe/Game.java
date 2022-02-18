c99eb30da188520a1e1246b6388f38e2c73e63ef
kgp23 <krishgpatel@gmail.com>
2022-02-18 13:57:22 -0500

    Created Game Class

---
+++ TikTacToe/src/tiktactoe/Game.java
@@ -0,0 +1,27 @@
+package tiktactoe;
+
+public class Game 
+{
+	
+	private Player[][] gameBoard;
+	
+	enum Player
+	{
+		X,
+		O
+	}
+	
+	public Game() 
+	{
+		gameBoard = new Player[3][3];
+	}
+	
+	
+	
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
