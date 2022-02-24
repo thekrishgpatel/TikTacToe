3a11de0c70797455b894877343918cb97f19eee9
enzeeo <enzonakornsri@gmail.com>
2022-02-23 21:27:02 -0500

    Update SmartComputerPlayer.java

--- TikTacToe/src/tiktactoe/SmartComputerPlayer.java
+++ TikTacToe/src/tiktactoe/SmartComputerPlayer.java
@@ -46,16 +46,65 @@ public class SmartComputerPlayer extends Player
 		
 		if ( winnerSymbol == this.symbol )
 		{
-			return 1;
+			return 1; // * availiableSpots
 		}
 		else if ( winnerSymbol != playerSymbol.Empty )
 		{
-			return -1;
+			return -1; // * availiableSpots
 		}
 		else
 		{
 			return 0;
 		}
+		
+		if (isMax) // comnputer player
+		{
+			int bestScore = Integer.MIN_VALUE;
+			
+			for ( int i = 0; i < 9; i++ )
+			{
+				// is available 
+				if ( game.isPositionValid(i) )
+				{
+					game.makeMove(this, i);
+					int score = minimax(game, availableSpots); // subtract 1
+					game.removePosition(i);
+					
+					if ( score > bestScore )
+					{
+						bestScore = score;
+					}
+					
+				}
+			}
+			
+			return bestScore; 
+		}
+		
+		else { // human player
+			
+			int bestScore = Integer.MAX_VALUE;
+			
+			for ( int i = 0; i < 9; i++ )
+			{
+				// is available 
+				if ( game.isPositionValid(i) )
+				{
+					game.makeMove(this, i);
+					int score = minimax(game, availableSpots); // subtract 1
+					game.removePosition(i);
+					
+					if ( score < bestScore )
+					{
+						bestScore = score;
+					}
+					
+				}
+			}
+			
+			return bestScore; 
+		}
+		
 //		
 //		if ( winnerSymbol != null )
 //		{
